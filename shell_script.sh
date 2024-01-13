7b9c647519454a8c0b8ef3852bd4f4c6b929ae73
AminaShahzad <128682413+AminaShahzad@users.noreply.github.com>
2024-01-13 20:04:08 +0500

    shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -4,7 +4,7 @@ cd ~/Desktop
 gcc try.c report_email_sender.c cJSON.c -o test -lcurl
 
 if [ $? -eq 0 ]; then
-  echo "Compilation successful. Waiting for 10 seconds before running the program..."
+ 
 
   while true; do
     # Run the compiled program
@@ -14,12 +14,14 @@ if [ $? -eq 0 ]; then
     # Check the exit code of the program
     if [ $exit_code -eq 0 ]; then
       echo "Program executed successfully."
+      echo " "
     else
       echo "Program exited with an error. Exit code: $exit_code"
     fi
 
     # Wait for the next iteration
     echo "Waiting for the next iteration..."
+    echo " "
     sleep 10
   done
 else
